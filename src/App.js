c43c6f3980786234d054726bd650c24256f35950
tcamargo10 <thiago.camargo@outlook.com>
2021-04-05 20:37:03 -0300

    create initial routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Containers
+import Layout from "./containers/Layout";
+
+// Pages
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" name="Login" component={Login} />
+          <Route exact path="/register" name="Register" component={Register} />
+          <Route path="/" name="Home" component={Layout} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
