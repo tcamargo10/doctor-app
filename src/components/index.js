8edcdf1517c1e06cc2f9c7bfecbff52c96a8e2fe
tcamargo10 <thiago.camargo@outlook.com>
2021-04-05 22:51:01 -0300

    container layout and components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import SidebarComponent from "./Sidebar";
+import ContentComponent from "./Content";
+import FooterComponent from "./Footer";
+import HeaderComponent from "./Header";
+
+export function Sidebar() {
+  return <SidebarComponent />;
+}
+export function Content() {
+  return <ContentComponent />;
+}
+export function Footer() {
+  return <FooterComponent />;
+}
+export function Header() {
+  return <HeaderComponent />;
+}
--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { Content, Sidebar, Footer, Header } from "../../components";
+import { Container, Wrapper, Body } from "./styles";
+
 export default function Layout() {
   return (
-    <div>
-      <h1>Layout</h1>
-    </div>
+    <Container>
+      <Sidebar />
+      <Wrapper>
+        <Header />
+        <Body>
+          <Content />
+        </Body>
+        <Footer />
+      </Wrapper>
+    </Container>
   );
 }
